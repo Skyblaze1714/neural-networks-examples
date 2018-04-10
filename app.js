d6f71ef090175abfbd9f2fd11a0373a86f2d4e44
Skyblaze1714 <danielelasagno@gmail.com>
2018-04-10 22:20:19 +0200

    Updated the kmh-2-ms example

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ net.train([{input: [1/12],    output: [1/3.33333]},
            {input: [1/1006],  output: [1/625.0994]},
            {input: [1/7],     output: [1/1.94444]},
            {input: [1/9],     output: [1/2.5]},
-           {input: [1/147],   output: [1/40.8333]}]);
+           {input: [1/147],   output: [1/40.8333]},
+           {input: [1/72],     output: [1/20]},
+           {input: [1/108],   output: [1/30]},
+           {input: [1/172.8],     output: [1/48]},
+           {input: [1/194.4],   output: [1/54]},
+           {input: [1/205.2],     output: [1/57]},
+           {input: [1/237.6],   output: [1/66]}]);
 
 //run the neural network with a value
 var output = net.run([1/8]);
