157108c346d56edfca4b3574044504340eb259ae
Skyblaze1714 <danielelasagno@gmail.com>
2018-04-10 22:04:13 +0200

    Added comments

--- km-2-miles/app.js
+++ km-2-miles/app.js
@@ -1,6 +1,7 @@
-//create a simple feed forward neural network with backpropagation
+//create a neural network
 var net = new brain.NeuralNetwork();
 
+//train the neural network
 net.train([{input: [1/50],    output: [1/31.0686]},
            {input: [1/20],    output: [1/12.4274]},
            {input: [1/14],    output: [1/8.6992]},
@@ -9,8 +10,10 @@ net.train([{input: [1/50],    output: [1/31.0686]},
            {input: [1/7],     output: [1/4.3496]},
            {input: [1/9],     output: [1/5.59234]}]);
 
+//run the neural network with a value
 var output = net.run([1/8]);  // [0.987]
 
+//map the value for output
 output = output.map((result) => {
   return 1/result;
 });
